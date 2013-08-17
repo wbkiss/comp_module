7d3ee4bccaadb1467cf1ab1f95cce306e63b9576
MadocComadrin <MadocComadrin@gmail.com>
2013-08-17 01:23:04 -0400

    Renamed X's map, updated quagmire, changed classifications
    X's map name: Candleroot Hills

--- module_scenes.py
+++ module_scenes.py
@@ -1228,53 +1228,53 @@ scenes = [
   ("multi_scene_enl_snowyhamlet",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001400389800003a4ea000058340000637a0000399b",
     [],[],"outer_terrain_snow"),
   
-  # CoMP "stable" maps
+  # CoMP Stable
   ("multi_scene_solace",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
-    [],[],"outer_terrain_plain"), # Solace (mixed) by Romans
+      [],[],"outer_terrain_plain"), # Solace (mixed) by Romans
   ("multi_scene_dry_valley",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000450000500000591660000431900001af3000077e3",
-    [],[],"outer_terrain_desert"), # Dry Valley (mixed) by Lord Rich (AZAN)
+      [],[],"outer_terrain_desert"), # Dry Valley (mixed) by Lord Rich (AZAN)
   ("multi_scene_verloren",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
-    [],[],"outer_terrain_plain"),
-   
-  # CoMP "testing" maps
+      [],[],"outer_terrain_plain"),
   
+  # CoMP Testing
   ("multi_scene_open_plain",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130000000000641900000bf4a00001d670000d869",
-    [],[], "outer_terrain_plain"), # Open Plain by KissMyAxe
+      [],[], "outer_terrain_plain"), # Open Plain (open) by KissMyAxe
   ("multi_scene_oasis",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001d00a9cba000651940000cf3f0000818300001f20",
-    [],[],"outer_terrain_desert"), # Oasis by Fehnor
+      [],[],"outer_terrain_desert"), # Oasis (open) by Fehnor
   ("multi_scene_snoops_exile_island",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300005000006f5bd00004ff5000004000000631b",
-    [],[], "outer_terrain_plain"), # Snoop's Exile Island (mixed) by Slytacular and Zaffa
-  
-  # CoMP "unstable" maps
-  
-  ("multi_scene_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000005300002840005795e0000bf0b000070a20000d9ae",
-    [],[], "outer_terrain_plain"), # Swamp by KissMyAxe
-  ("multi_scene_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
-    [],[],"outer_terrain_beach"), # Delta by Vendigr
-  ("multi_scene_liberty",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b0000500000350d600005d4600003c3100006cf6",
-    [],[], "outer_terrain_beach"), # Liberty (mixed/closed) by Romans
-  ("multi_scene_haranaer",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000400032320003c0f300001f9e000011180000031c",
-    [],[], "outer_terrain_desert"), # Haranaer (open) by Sotamursu
-  ("multi_scene_wold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000012002a0b20004992700006e54000007fe00001fd2",
-    [],[], "outer_terrain_steppe"), # The Wold (open) by Sotamursu
-  ("multi_scene_urabhaya",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000005000d795000350d4000011a4000017ee0000440d",
-    [],[], "outer_terrain_plain"), # Urabhaya (closed) by Sotamursu
-  ("multi_scene_rock_port",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000032224e0a800435130000794a0000305500001978",
-    [],[], "outer_terrain_plain"), # Rock Port (mixed) by Romans
-  ("multi_scene_river_valley",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300c93c800033cce000020d5000065c70000071b",
-    [],[], "outer_terrain_plain"), # River Valley (mixed) by Vendigr
-  ("multi_scene_shariz_village",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000003165588c0004c92d0000538c000052de000043b4",
-    [],[], "outer_terrain_steppe"), # Shariz Village (closed) by Maynd
-  ("multi_scene_old_rhodok_stronghold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b000182a000408fe0000666d00007db9000015ee",
-    [],[], "outer_terrain_plain"), # Old Rhodok Stronghold (closed) by Erminas
-  ("multi_scene_abbey",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000a002191b0006a5a5000071310000576d00002a84",
-    [],[], "outer_terrain_steppe"), # The Abbey (mixed) by Erminas
+      [],[], "outer_terrain_plain"), # Snoop's Exile Island (mixed) by Slytacular and Zaffa
   ("multi_scene_low_born",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000003000d732000350e4000047ce00004ba700005d54",
-    [],[], "outer_terrain_plain"), # Low Born (Closed) by Romans
+      [],[], "outer_terrain_plain"), # Low Born (closed) by Romans
+  ("multi_scene_shariz_village",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000003165588c0004c92d0000538c000052de000043b4",
+      [],[], "outer_terrain_steppe"), # Shariz Village (closed) by Maynd
+  ("multi_scene_liberty",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b0000500000350d600005d4600003c3100006cf6",
+      [],[], "outer_terrain_beach"), # Liberty (mixed/closed) by Romans
+  ("multi_scene_urabhaya",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000005000d795000350d4000011a4000017ee0000440d",
+      [],[], "outer_terrain_plain"), # Urabhaya (closed) by Sotamursu
+  ("multi_scene_rock_port",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000032224e0a800435130000794a0000305500001978",
+      [],[], "outer_terrain_plain"), # Rock Port (mixed) by Romans
+  	
+  # CoMP Unstable
+  ("multi_scene_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
+      [],[],"outer_terrain_beach"), # Delta (open) by Vendigr
+  ("multi_scene_abbey",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000a002191b0006a5a5000071310000576d00002a84",
+      [],[], "outer_terrain_steppe"), # The Abbey (mixed) by Erminas
+  ("multi_scene_river_valley",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300c93c800033cce000020d5000065c70000071b",
+      [],[], "outer_terrain_plain"), # River Valley (mixed) by Vendigr
   ("multi_scene_quagmire",sf_generate|sf_muddy_water,"none", "none", (0,0),(100,100),-100,"0x00000000b00005000003d8f200007f6d00000fcf00004a0a",
-    [],[],"outer_terrain_plain"), # Quamire (Open) by Madoc Comadrin
-  ("multi_scene_x",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
-    [],[],"outer_terrain_plain"),
+      [],[],"outer_terrain_plain"), # Quamire (Open) by Madoc Comadrin
+  ("multi_scene_candleroot_hills",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
+      [],[],"outer_terrain_plain"), #Candleroot Hills (Open) by Mr. X
+  	
+  #Waiting/Needs Work
+  ("multi_scene_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000005300002840005795e0000bf0b000070a20000d9ae",
+      [],[], "outer_terrain_plain"), # Swamp (open/mixed) by KissMyAxe
+  ("multi_scene_haranaer",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000400032320003c0f300001f9e000011180000031c",
+      [],[], "outer_terrain_desert"), # Haranaer (open) by Sotamursu
+  ("multi_scene_wold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000012002a0b20004992700006e54000007fe00001fd2",
+      [],[], "outer_terrain_steppe"), # The Wold (open) by Sotamursu
+  ("multi_scene_old_rhodok_stronghold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b000182a000408fe0000666d00007db9000015ee",
+      [],[], "outer_terrain_plain"), # Old Rhodok Stronghold (closed) by Erminas
   #CoMP-end
 
   ("multiplayer_maps_end",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
