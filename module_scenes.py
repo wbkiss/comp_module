aa2b0bc900d38b6a01c22431c29e6b0c998c5a36
MadocComadrin <MadocComadrin@gmail.com>
2013-09-28 15:12:16 -0400

    Updated Maps; added Suddenly
    Maps updated: Dreiecksdorf, Liberty, Rock Port, Sally Out, Island Resort
    (SEI), Solace.

--- module_scenes.py
+++ module_scenes.py
@@ -1251,7 +1251,11 @@ scenes = [
   ("multi_scene_urabhaya",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000005000d795000350d4000011a4000017ee0000440d",
       [],[], "outer_terrain_plain"), # Urabhaya (closed) by Sotamursu
   ("multi_scene_rock_port",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000230024e0a800435130000794a0000305500001978",
-      [],[], "outer_terrain_plain"), # Rock Port (mixed) by Romans
+      [],[], "outer_terrain_beach"), # Rock Port (mixed) by Romans
+  ("multi_scene_quagmire",sf_generate|sf_muddy_water,"none", "none", (0,0),(100,100),-100,"0x00000000b00005000003d8f200007f6d00000fcf00004a0a",
+      [],[],"outer_terrain_plain"), # Quamire (Open) by Madoc
+  ("multi_scene_the_farm",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000033000053d000735cd00001e2100004c8e000039a9",
+    [],[],"outer_terrain_plain"), #The Farm (Open) by Romans
   	
   # CoMP Unstable
   ("multi_scene_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
@@ -1260,19 +1264,17 @@ scenes = [
       [],[], "outer_terrain_steppe"), # The Abbey (mixed) by Erminas
   ("multi_scene_river_valley",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300c93c800033cce000020d5000065c70000071b",
       [],[], "outer_terrain_plain"), # River Valley (mixed) by Vendigr
-  ("multi_scene_quagmire",sf_generate|sf_muddy_water,"none", "none", (0,0),(100,100),-100,"0x00000000b00005000003d8f200007f6d00000fcf00004a0a",
-      [],[],"outer_terrain_plain"), # Quamire (Open) by Madoc
   ("multi_scene_candleroot_hills",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
       [],[],"outer_terrain_plain"), #Candleroot Hills (Open) by Mr. X
   ("multi_scene_lake_fort",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300322930006c5b1000005ad000056ed00002787",
     [],[],"outer_terrain_plain"), #Lake Fort (Mixed) by Erminas
-  ("multi_scene_the_farm",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000033000053d000735cd00001e2100004c8e000039a9",
-    [],[],"outer_terrain_plain"), #The Farm (Open) by Romans
   ("multi_scene_dreiecksdorf",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300000000005ad68000041ef00005ae800003c55",
     [],[],"outer_terrain_plain"), #Dreiecksdorf (Closed) by Madoc
   ("multi_scene_sally_out",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300001020004fd40000041ef00005ae800003c55",
     [],[],"outer_terrain_plain"),
-  	
+  ("multi_scene_suddenly",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000345044a9d00036cdf00005a8f0000538d00003871",
+    [],[],"outer_terrain_snow"),
+
   #Waiting/Needs Work
   ("multi_scene_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000005300002840005795e0000bf0b000070a20000d9ae",
       [],[], "outer_terrain_plain"), # Swamp (open/mixed) by KissMyAxe
